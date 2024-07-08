da6c3f1f4cc1b4ff13b69115696fec08939291a5
MooWantFree <i@moo.ac>
2024-07-09 04:57:36 +0800

    feat: Show table content from Counter api

--- src/pages/manage/counter/Counter.tsx
+++ src/pages/manage/counter/Counter.tsx
@@ -10,29 +10,30 @@ import {
 } from "@hope-ui/solid"
 import { PageResp } from "~/types"
 import { handleResp, r } from "~/utils"
-import { createEffect, createSignal, For } from "solid-js"
+import { createEffect, createSignal } from "solid-js"
 
 interface CounterResp {
-  id: number // 文件 ID
-  file_name: string // 文件名
-  file_path: string // 文件路径
-  time: string // 时间戳
-  ip_address: string // IP 地址
-  status_code: number // 状态码
+  id: number
+  file_name: string
+  file_path: string
+  time: string
+  ip_address: string
+  status_code: number
 }
 
 const Counter = () => {
   const [counters, setCounters] = createSignal<CounterResp[]>([])
+  const [reverse, setReverse] = createSignal(false)
+  const [sortKey, setSortKey] = createSignal("time")
   let current_page = 1
   let page_size = 1111
-  let time = "time"
-  let reverse = false
+
   const getCounters = async () => {
     const resp: PageResp<CounterResp> = await r.post("/admin/counter/get", {
       current_page: current_page,
       page_size: page_size,
-      sort_key: time,
-      reverse: reverse,
+      sort_key: sortKey(),
+      reverse: reverse(),
     })
     handleResp(resp, (data) => setCounters(data.content))
   }
@@ -41,29 +42,66 @@ const Counter = () => {
     getCounters()
   })
 
+  const handleHeaderClick = (key: string) => {
+    if (sortKey() === key) {
+      setReverse(!reverse())
+    } else {
+      setSortKey(key)
+      setReverse(false)
+    }
+  }
+
   return (
     <Table highlightOnHover dense>
       <TableCaption>DownloadCounter</TableCaption>
       <Thead>
         <Tr>
-          <Th>id</Th>
-          <Th>file_name</Th>
-          <Th>file_path</Th>
-          <Th>time</Th>
-          <Th>ip_address</Th>
-          <Th>status_code</Th>
+          <Th
+            onClick={() => handleHeaderClick("id")}
+            style={{ cursor: "pointer" }}
+          >
+            id
+          </Th>
+          <Th
+            onClick={() => handleHeaderClick("file_name")}
+            style={{ cursor: "pointer" }}
+          >
+            file_name
+          </Th>
+          <Th
+            onClick={() => handleHeaderClick("file_path")}
+            style={{ cursor: "pointer" }}
+          >
+            file_path
+          </Th>
+          <Th
+            onClick={() => handleHeaderClick("time")}
+            style={{ cursor: "pointer" }}
+          >
+            time
+          </Th>
+          <Th
+            onClick={() => handleHeaderClick("ip_address")}
+            style={{ cursor: "pointer" }}
+          >
+            ip_address
+          </Th>
+          <Th
+            onClick={() => handleHeaderClick("status_code")}
+            style={{ cursor: "pointer" }}
+          >
+            status_code
+          </Th>
         </Tr>
       </Thead>
       <Tbody>
-        <For each={counters()}>
-          {(drive) => (
-            <Tr>
-              {Object.values(drive).map((value, index) => (
-                <Td>{value}</Td>
-              ))}
-            </Tr>
-          )}
-        </For>
+        {counters().map((drive) => (
+          <Tr>
+            {Object.values(drive).map((value, index) => (
+              <Td>{value}</Td>
+            ))}
+          </Tr>
+        ))}
       </Tbody>
       <Tfoot>
         <Tr>
@@ -74,5 +112,4 @@ const Counter = () => {
   )
 }
 
-// 导出组件
 export default Counter
